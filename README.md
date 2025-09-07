b215a54da6edf9fc391318c6d9ae005a6a5b1cf1
Dariusz Gogacz <dariusz.gogacz@yahoo.pl>
2025-09-07 09:19:30 +0200

    Upgrade

--- README.md
+++ README.md
@@ -1,43 +1,117 @@
 # FrSky Telemetry Box
+
 Read data from HobbyWing ESCs and send it via FrSky Smart Port to Your TX.
 
+## ESP32 Board
+
+Telemetry box uses ESP32 board. Details:
+
+- VIN: 3.3-12V gives very wide voltage range
+
+- GPIO32: Connected to Hobbywing ESC for telemetry data.
+
+- GPIO33: It is SmartPort RX signal. It reads the line. Connect it to model's receiver SmartPort
+
+- GPIO14: It is SmartPort TX signal. It is used to transmit data to receiver's SmartPort. Use 120-220ohm resistor. After resistor - join it with GPIO33 and this needs to be bi-directional communication.
+  
+  
+  
+  Pinout:
+  
+  ![pinout.jpg](/images/pinout.jpg)
+  
+  
+
 ## HobbyWing ESC
+
 Communication details:
+
 - 19 200 bps
+- Parity bit: No
+- Stop bit: 1
 - Big endian coding
-- bit duration: 52 us
-- two types of frames sent: ID, data
-- Normal logic
+- Bit duration: 52 us
+- Two types of frames sent
+  - Hobbywing signature frame (13 bytes)
+  - Data frame (19-20bytes)
+- Logic: normal
+  
+  
 
 Timechart:
-![Hobbywing](https://github.com/g0rd0n2007/frsky-tb/blob/main/Zrzut%20ekranu%20z%202023-10-01%2012-06-21.png)
-
-Frame with ID:
-|                | 1    | 2    | 3    | 4    | 5    | 6    | 7    | 8    | 9    | 10   | 11   | 12   | 13   |
-| -------------- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- |
-| V4LV/25/60/80A | 0x9b | 0x9b | 0x03 | 0xe8 | 0x01 | 0x08 | 0x5b | 0x00 | 0x01 | 0x00 | 0x21 | 0x21 | 0xb9 |
-| V4HV200A OPTO  | 0x9b | 0x9b | 0x03 | 0xe8 | 0x01 | 0x02 | 0x0d | 0x0a | 0x3d | 0x05 | 0x1e | 0x21 | 0xb9 |
-| V5HV130A OPTO  | 0x9b | 0x9b | 0x03 | 0xe8 | 0x01 | 0x0b | 0x41 | 0x21 | 0x44 | 0xb9 | 0x21 | 0x21 | 0xb9 |
-| HW HV 200A     | 0x9b | 0x9b | 0x02 | 0xd0 | 0x01 | 0x0b | 0x41 | 0x21 | 0x7e | 0x62 | 0x21 | 0x21 | 0xb9 |
-| HW 120A        | 0x9b | 0x9b | 0x03 | 0xe8 | 0x01 | 0x08 | 0x5b | 0x21 | 0x71 | 0x6e | 0x21 | 0x21 | 0xb9 |
-
-Frame with data:
-| Byte        | 1    | 2-4          | 5-6    | 7-8     | 9-11         | 12-13   | 14-15   | 16-17    | 18-19 |
-| ----------- |  --- | ------------ | ------ | ------- | ------------ | ------- | ------- | -------- | ----- |
-| Description | Head | Frame number | Rx Thr | Out PWM | RPM raw data | Voltage | Current | Temp FET | Temp  |
-| Value       | 0x9b |              | 0-1023 | 0-1023  |              |         |         |          |       |
 
 
-Rx Thr, Out PWM: 
-- 0-1023
-  
-RPM:
--	Motor RPM = RPM_raw_data / No_of_pole_pairs (set it in TX)
+![1.png](/home/dariusz-gogacz/Dokumenty/frsky-tb/images/1.png)
+
+
+
+### Data frames
+
+Length equal to 19-20 bytes. 
+
+- 0: Start byte 0x9B
+
+- 1-3: Frame number
+
+- 4-5: 0-1000 means 0-100% of RX Throttle. Divide this value by 10.0 to get percent value.
+
+- 6-7: 0-1000 means 0-100% of Motor PWM output. Divide this value by 10.0 to get percent value.
+
+- 8-10: RPM raw value. Motor RPM = RPM_raw_data / No_of_pole_pairs (set it in TX, usualy equal to 5)
+
+- 11-12: Voltage (raw)
+
+- 13-14: Current (raw)
+
+- 15-16: Temperature 1
+
+- 17-18: Temperature 2
+
+- 19: Optional. Present when motor is off. Equal to 0xB9
+
+
+
+Examples:
+
+|      | Start | Frame number | RX Throttle | Output PWM | RPM    | Voltage | Current | Temp1 | Temp2 | End  |
+| ---- | ----- | ------------ | ----------- | ---------- | ------ | ------- | ------- | ----- | ----- | ---- |
+| Byte | 0     | 1-3          | 4-5         | 6-7        | 8-10   | 11-12   | 13-14   | 15-16 | 17-18 | 19   |
+|      | 0x9B  | 0007DC       | 012C        | 0000       | 000000 | 0A49    | 0000    | 0C80  | 0C98  | 0xB9 |
+|      | 0x9B  | 0007DD       | 012C        | 0000       | 000000 | 0A4A    | 0000    | 0C80  | 0C97  | 0xB9 |
+|      | 0x9B  | 0007DE       | 012C        | 0000       | 000000 | 0A49    | 0000    | 0C80  | 0C98  | 0xB9 |
+|      | 0x9B  | 0007DF       | 012C        | 0000       | 000000 | 0A4A    | 0000    | 0C80  | 0C97  | 0xB9 |
+|      | 0x9B  | 0007E0       | 012C        | 0000       | 000000 | 0A48    | 0000    | 0C7E  | 0C98  | 0xB9 |
+|      | 0x9B  | 0007E1       | 012C        | 0000       | 000000 | 0A4C    | 0000    | 0C82  | 0C9A  | 0xB9 |
+|      | 0x9B  | 0007E2       | 012C        | 0000       | 000000 | 0A48    | 0000    | 0C80  | 0C96  | 0xB9 |
+|      | 0x9B  | 0007E3       | 012C        | 0000       | 000000 | 0A48    | 0000    | 0C80  | 0C95  | 0xB9 |
+|      | 0x9B  | 0007E4       | 012C        | 0000       | 000000 | 0A4B    | 0000    | 0C80  | 0C98  | 0xB9 |
+|      | 0x9B  | 0007E5       | 012C        | 0000       | 000000 | 0A48    | 0000    | 0C80  | 0C9A  | 0xB9 |
+|      | 0x9B  | 0007E6       | 012C        | 0000       | 000000 | 0A49    | 0000    | 0C80  | 0C98  | 0xB9 |
+|      | 0x9B  | 0007E7       | 012C        | 0000       | 000000 | 0A48    | 0000    | 0C80  | 0C98  | 0xB9 |
+
+
+
+### Signature frames
+
+Length equal to 13 bytes. But it sends two bytes 0x9B and my software sees it as 12 byte frame. I use 0x9B to trigger analysis and therefore I analyse 12 bytes.
+
+
+
+|                | Start | Start |      |      |      |      |      |      |      |      |      |      | End  |
+| -------------- | ----- | ----- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- | ---- |
+| Byte           | -1    | 0     | 1    | 2    | 3    | 4    | 5    | 6    | 7    | 8    | 9    | 10   | 11   |
+| V4LV/25/60/80A | 0x9B  | 0x9B  | 0x03 | 0xE8 | 0x01 | 0x08 | 0x5B | 0x00 | 0x01 | 0x00 | 0x21 | 0x21 | 0xB9 |
+| V4HV200A OPTO  | 0x9B  | 0x9B  | 0x03 | 0xE8 | 0x01 | 0x02 | 0x0D | 0x0A | 0x3D | 0x05 | 0x1E | 0x21 | 0xB9 |
+| V5HV130A OPTO  | 0x9B  | 0x9B  | 0x03 | 0xE8 | 0x01 | 0x0B | 0x41 | 0x21 | 0x44 | 0xB9 | 0x21 | 0x21 | 0xB9 |
+| HW HV 200A     | 0x9B  | 0x9B  | 0x02 | 0xD0 | 0x01 | 0x0B | 0x41 | 0x21 | 0x7E | 0x62 | 0x21 | 0x21 | 0xB9 |
+| HW 120A        | 0x9B  | 0x9B  | 0x03 | 0xE8 | 0x01 | 0x08 | 0x5B | 0x21 | 0x71 | 0x6E | 0x21 | 0x21 | 0xB9 |
+
 
 
 ## FrSky Smart Port
 
 Communication details:
+
 - 57 600 bps
 - Little endian coding
 - bit duration: 17 us
@@ -60,9 +134,11 @@ If sensor is present - it answers with:
 |      | 0x10 |             |       |     |
 
 Pooled sensor IDs:
+
 - 6A CB AC 0D 8E 2F D0 71 F2 53 34 95 16 B7 98 39 BA 1B 00 A1 22 83 E4 45 C6 67 48 E9
 
 Known sensor types:
+
 - RPM: 0x0500
 - A3 (Voltage): 0x0900
 - A4: 0x0910
@@ -72,10 +148,11 @@ Known sensor types:
 - FLVV Cell sensor: 0x0300
 
 CRC Calculation code:
+
 ```C
 uint8_t CalculateCRC(uint8_t bytes[], int len){
   short crc = 0;
-  
+
   for(int i=0;i<len;i++) {
     crc += bytes[i]; //0-1FF
     crc += crc >> 8;  //0-100
@@ -88,6 +165,7 @@ uint8_t CalculateCRC(uint8_t bytes[], int len){
 ```
 
 Bytes sequence imitating FLVV Cell sensor for 2S LiPo:
+
 - 0x10, 0x00, 0x03, 0x20, 0x2c, 0xc8, 0x82
 - First is the head
 - Sensor type is 0x0300,
---
+++ frsky-tb.ino
@@ -0,0 +1,336 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+#include "BluetoothSerial.h"
+
+const char* TB_Version = "0.1.3";
+
+HardwareSerial escSerial(1);
+HardwareSerial frskySerial(2);
+BluetoothSerial bluetoothSerial;
+
+const int escRX_PIN = 32;
+const int frskyRX_PIN = 33;
+const int frskyTX_PIN = 14;
+const int LED_PIN = 2;
+
+int HobbywingSignatureIdx = -1;
+float Thr = 0.0;
+float OutPWM = 0.0;
+float Voltage = 0.0;
+float Temp1 = 0.0;
+float Temp2 = 0.0;
+uint32_t RPM = 0;
+float Current = 0.0;
+uint16_t CurrentOffset = 0;
+
+#define MAX_FRAME_SIZE 20
+uint8_t escFrame[MAX_FRAME_SIZE];
+uint8_t escFrameIndex = 0;
+uint8_t frskyFrame[MAX_FRAME_SIZE];
+uint8_t frskyFrameIndex = 0;
+
+//Free FrSky sensor ID that are not connected to line
+uint8_t freeIDs[255] = {0};
+uint8_t freeIDidx = 0;
+
+#define U16(i) ((buf[i] << 8) | buf[i+1])
+#define U24(i) ((buf[i] << 16) | (buf[i+1] << 8) | (buf[i+2]))
+#define U32(i) ((buf[i] << 24) | (buf[i+1] << 16) | (buf[i+2] << 8) | (buf[i+3]))
+
+struct Signature {
+  byte Sig[12];
+  const char* Name;
+  float VoltageDivisor;
+  float CurrentDivisor;
+};
+
+Signature Signatures[] = {
+  {{0x9b,	0x02,	0xd0,	0x01,	0x0b,	0x41,	0x21,	0x7e,	0x62,	0x21,	0x21,	0xb9}, "Hobbywing 200A", 58.0,  3.103662322},
+  {{0x9b,	0x03,	0xe8,	0x01,	0x08,	0x5b,	0x21,	0x71,	0x6e,	0x21,	0x21,	0xb9}, "Hobbywing 120A", 113.0, 3.103662322}
+};
+
+const int signaturesCount = sizeof(Signatures) / sizeof(Signatures[0]);
+
+const int checkSignature(byte* buf) {
+  for (int i = 0; i < signaturesCount; i++) {
+    bool match = true;
+    for (int j = 0; j < 12; j++) {
+      if (buf[j] != Signatures[i].Sig[j]) {
+        match = false;
+        break;
+      }
+    }
+    if (match) {
+      return i;
+    }
+  }
+
+  return -1; // brak dopasowania
+}
+
+void printFrame(const char* str, uint8_t* buf, uint8_t len) {
+  Serial.printf("%s ", str);
+  for(int i=0; i<len; i++) Serial.printf("%02X ", buf[i]);
+  Serial.println();
+}
+
+bool idIsFree(uint16_t id){
+  for(int i=0; i<freeIDidx;i++){
+    if(freeIDs[i]==id)return false;
+  }
+  return true;
+}
+
+
+float calcTemp(uint16_t temp_raw){
+  const float Vref = 3.3f;
+  const float Rfixed = 283000.0f;//2800.0f;//200000.0f;   // << dobrany ~200 kΩ
+  const float R0 = 10000.0f;        // NTC 10k @25°C
+  const float B  = 3950.0f;
+
+  float voltage    = (temp_raw / 4095.0f) * Vref;
+  float resistance = Rfixed * voltage / (Vref - voltage);
+  //float resistance = Rfixed * (Vref - voltage) / voltage;
+  float kelvin     = 1.0f / ((1.0f/298.15f) + (1.0f/B)*log(resistance/R0));
+  return kelvin - 273.15f; // °C
+}
+
+// Funkcja dekodująca dane telemetryczne
+void decodeHwFrame(uint8_t* buf, uint8_t len) {
+  if (len < 10) return; // minimalna długość ramki
+
+  //printFrame("[HW Frame]", buf, len);
+
+  uint32_t frame_id = U24(1);
+  uint16_t rx_thr = U16(4);
+  Thr = rx_thr / 10.0;
+  uint16_t out_pwm_raw = U16(6);
+  OutPWM = out_pwm_raw / 10.0;
+
+  uint32_t rpm_raw = U24(8);
+  RPM = rpm_raw;
+
+  uint16_t voltage_raw = U16(11);
+  uint16_t current_raw = U16(13);
+  
+  if(HobbywingSignatureIdx >= 0) {
+    Voltage = voltage_raw / Signatures[HobbywingSignatureIdx].VoltageDivisor;
+  }
+
+  
+  if(CurrentOffset==0 && current_raw>0){
+    CurrentOffset=current_raw;
+  }
+  if((current_raw - CurrentOffset) > 0 && HobbywingSignatureIdx>=0){
+    Current = (current_raw - CurrentOffset) / Signatures[HobbywingSignatureIdx].CurrentDivisor;
+  }else{
+    Current = 0.0;
+  }
+
+  uint8_t temp1 = U16(15);  
+  uint8_t temp2 = U16(17);  
+  if(temp1 > 0) Temp1 = calcTemp(temp1);
+  if(temp2 > 0) Temp2 = calcTemp(temp2);
+  
+  //Serial.printf("%u\t%3.2f %%\t%3.2f %%\t%2.2f [V]\t%3.2f [A]\t%u\t%u\n", frame_id, thr, out_pwm, voltage, current, temp1, temp2);
+  /*string msg = string.format()
+  Serial.printf("%u\t%3.2f\t%3.2f\t", frame_id, Thr, OutPWM);
+  Serial.printf("%3.2f\t%u\t", Voltage, voltage_raw);
+  Serial.printf("%3.2f\t%u\t", Current, current_raw);
+  Serial.printf("%2.2f\t%u\t%2.2f\t%u\t", Temp1, temp1, Temp2, temp2);  
+  Serial.printf("%u\n", RPM);*/
+
+  char msg[256] = "";
+  snprintf(msg, sizeof(msg),
+    "%u\t%.2f\t%.2f\t%.2f\t%u\t%.2f\t%u\t%.2f\t%u\t%.2f\t%u\t%u\n",
+    frame_id, Thr, OutPWM,
+    Voltage, voltage_raw,
+    Current, current_raw,
+    Temp1, temp1, Temp2, temp2,
+    RPM);
+
+  
+  Serial.print(msg);
+  bluetoothSerial.print(msg);
+}
+
+
+// Funkcja obliczająca sumę kontrolną CRC16
+uint8_t calcCrc(const uint8_t* buf, size_t len) {
+  short crc = 0;
+  
+  for(int i=0;i<len;i++) {
+    crc += buf[i]; //0-1FF
+    crc += crc >> 8;  //0-100
+    crc &= 0x00ff;
+    crc += crc >> 8;  //0-0FF
+    crc &= 0x00ff;
+  }
+  return ~crc;
+}
+
+void sendToSmartPort(uint16_t type, uint32_t value){
+  uint8_t buf[8];
+
+  buf[0]=0x10;
+  
+  buf[1]=type & 0x00ff;
+  buf[2]=(type >> 8) & 0x00ff;
+
+  buf[3]=(value >> 0) & 0x000000ff;
+  buf[4]=(value >> 8) & 0x000000ff;
+  buf[5]=(value >> 16) & 0x000000ff;
+  buf[6]=(value >> 24) & 0x000000ff;
+
+  buf[7]=calcCrc(buf, 7);
+
+  frskySerial.write(buf, 8);
+}
+
+
+void setup() {
+  bluetoothSerial.begin("TelemetryBox BT");
+
+  //pinMode(escRX_PIN, INPUT);
+  //pinMode(frskyRX_PIN, INPUT);
+  //pinMode(frskyTX_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  //sleep(2);
+
+  Serial.begin(115200);
+  escSerial.begin(19200, SERIAL_8N1, escRX_PIN);//Hobbywing  
+  frskySerial.begin(57600, SERIAL_8N1, frskyRX_PIN, frskyTX_PIN, true);//FrSky Smart Port
+  
+  Serial.printf("=== Welcome in Telemetry Box v%s ===\n", TB_Version);
+}
+
+void loop() {
+  if(escSerial.available() > 0) {
+    uint8_t b = escSerial.read();
+    //Serial.printf("%02X ", b);
+
+    if (b == 0x9B) {
+      if (escFrameIndex > 0) {        
+        if (
+          escFrameIndex == 19 || escFrameIndex == 20                    
+        ) {
+          //printFrame("[HW Frame]", escFrame, escFrameIndex);
+          //digitalWrite(LED_PIN, HIGH);
+          decodeHwFrame(escFrame, escFrameIndex);          
+          //digitalWrite(LED_PIN, LOW);
+        }else if(escFrameIndex == 12){
+          //Signature frame
+          if(HobbywingSignatureIdx<0){
+            printFrame("[HW Frame SIG]", escFrame, escFrameIndex);
+            HobbywingSignatureIdx = checkSignature(escFrame);
+            if(HobbywingSignatureIdx>=0) Serial.printf("Signature found for %s\n", Signatures[HobbywingSignatureIdx].Name);
+          }
+        }else if(escFrameIndex == 1){
+          //Ignore
+        }else{
+          Serial.printf("[HW Frame] Błąd: ramka o długości %u\n", escFrameIndex);
+          printFrame("[HW Frame]", escFrame, escFrameIndex);
+        }
+      }
+      escFrameIndex = 0; // nowa ramka
+    }
+
+    if (escFrameIndex < MAX_FRAME_SIZE) {
+      escFrame[escFrameIndex++] = b;
+    } else {
+      escFrameIndex = 0; // reset przy przepełnieniu
+      //Serial.println(/*"Błąd: ramka za długa"*/);
+    }
+  }
+
+
+
+
+
+
+
+  if(frskySerial.available() > 0){
+    uint8_t b = frskySerial.read();
+    //Serial.printf("%02X ", b);
+
+    if (b == 0x7E) {
+      if(frskyFrameIndex > 0) {
+        if (frskyFrameIndex == 2) {
+          //Serial.printf("Odpytanie o czujnik %02X a czujnik nie odpowiedział\n", frskyFrame[1]);
+
+          int i=0;
+          bool found=false;
+          for(i=0; i<freeIDidx && i<255; i++){
+            if(freeIDs[i]==frskyFrame[1]){
+              found=true;
+              break;
+            }
+          }
+
+          if(!found && i<255) {
+            freeIDs[freeIDidx++]=frskyFrame[1];            
+            Serial.printf("Dodawanie wolnego ID FrSky: %02X (%u dodanych ID)\n", frskyFrame[1], freeIDidx);
+          }
+        }else if(frskyFrameIndex == 8){
+          Serial.printf("Odpytanie o czujnik %02X - czujnik obecny na linii\n", frskyFrame[1]);
+          //Serial.printf("[FrSky Frame] ");
+          //for(int i=0; i< frskyFrameIndex; i++) Serial.printf("%02X ", frskyFrame[i]);
+          //Serial.println();
+          printFrame("[FrSky Frame]", frskyFrame, frskyFrameIndex);
+        }
+      }
+      frskyFrameIndex = 0;
+    }
+
+    if (frskyFrameIndex < MAX_FRAME_SIZE) {
+      frskyFrame[frskyFrameIndex++] = b;
+
+      //Respond
+      if(frskyFrameIndex==2 && freeIDidx>=10){
+        digitalWrite(LED_PIN, LOW);
+        //if(frskyFrame[1]==freeIDs[0]) sendToSmartPort(0x0300, 0x82c82c20);//Cells sensor
+        if(frskyFrame[1]==0x00){
+          if(idIsFree(0x00)) sendToSmartPort(0x0900, Voltage * 100.0);//Cells sensor)
+          else {
+            Serial.printf("[FrSky] 0x00 zajęte\n");
+            digitalWrite(LED_PIN, HIGH);
+          }
+        } 
+        if(frskyFrame[1]==0xa1){
+          if(idIsFree(0xa1)) sendToSmartPort(0x0400, Temp1);//Temp1
+          else {
+            Serial.printf("[FrSky] 0xa1 zajęte\n");
+            digitalWrite(LED_PIN, HIGH);
+          }
+        }
+        if(frskyFrame[1]==0x22){
+          if(idIsFree(0x22)) sendToSmartPort(0x0410, Temp2);//Temp1)
+          else {
+            Serial.printf("[FrSky] 0x22 zajęte\n");
+            digitalWrite(LED_PIN, HIGH);
+          }
+        }
+        if(frskyFrame[1]==0x83){
+          if(idIsFree(0x83)) sendToSmartPort(0x0500, RPM);//RPM
+          else {
+            Serial.printf("[FrSky] 0x83 zajęte\n");
+            digitalWrite(LED_PIN, HIGH);
+          }
+        }
+        if(frskyFrame[1]==0xe4){
+          if(idIsFree(0xe4)) sendToSmartPort(0x0200, Current);//Current
+          else {
+            Serial.printf("[FrSky] 0xe4 zajęte\n");
+            digitalWrite(LED_PIN, HIGH);
+          }
+        }
+      }      
+    } else {
+      frskyFrameIndex = 0;
+    }
+  }
+  
+}
+
